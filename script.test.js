b0bcb7310f9fd2901e4138004a761387cc74b468
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-23 10:32:51 +0000

    testing add and greetUser function

--- script.test.js
+++ script.test.js
@@ -1,1 +1,9 @@
-const {add, greetUser} = require('./script');
+const { add, greetUser } = require("./script");
+
+test("Greet user", () => {
+  expect(greetUser("Sam")).toBe("Hello, Sam!");
+});
+
+test("Add two numbers", () => {
+    expect(add(1, 2)).toBe(3);
+});
